71d59e587b9af7c5921f5c055c5260e1f1677f26
rodritechsp <44809999+rodritechsp@users.noreply.github.com>
2026-01-14 21:38:00 -0300

    Add files via upload
    case sensitive

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { HashRouter, Routes, Route } from 'react-router-dom';
-import { AudioProvider } from './context/AudioContext';
+import { AudioProvider } from './context/AudioProvider';
 import Layout from './components/Layout';
 import HomePage from './pages/HomePage';
 import ExplorePage from './pages/ExplorePage';
